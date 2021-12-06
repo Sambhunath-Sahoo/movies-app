1de411b1fe8aa79918bb402e4aab303266a6dbb8
Sambhunath-Sahoo <sambhunathsahoo25@gmail.com>
2021-12-06 16:07:30 +0530

    navbar updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-export default class Movies extends Component {
+export default class Navbar extends Component {
     render() {
         return (
-            <div style={{display: 'flex', padding: '0.5'}}>
-                <h1>Movies App</h1>
-                <h2 style={{marginLeft: '2rem', marginTop: '1.5rem'}}>Favourites</h2>
+            <div style={{ display: 'flex', padding: '0.5' }}>
+                <Link to="/" style={{ textDecoration: 'none' }}>
+                    <h1 style={{ marginTop: '1rem', marginLeft: '1rem' }}>Movies App</h1>
+                </Link>
+                <Link to="/favourites" style={{ textDecoration: 'none' }}>
+                    <h2 style={{ marginLeft: '2rem', marginTop: '1.5rem' }}>Favourites</h2>
+                </Link>
             </div>
         )
     }
