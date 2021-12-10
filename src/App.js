f089de113853f9b369fc00e00c984c92c07ef83f
Sambhunath-Sahoo <sambhunathsahoo25@gmail.com>
2021-12-10 15:06:29 +0530

    Banner UI completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import Navbar from './components/Navbar';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Banner from "./components/Banner";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Banner />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
