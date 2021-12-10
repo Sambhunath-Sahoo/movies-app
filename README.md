c8571052a3b08c27cf1250ef63324f8c3bb9233b
Sambhunath-Sahoo <sambhunathsahoo25@gmail.com>
2021-12-10 21:54:14 +0530

    API , changeMovie, Pagination Implemented

--- README.md
+++ README.md
@@ -4,6 +4,44 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 ## Available Scripts
 
+<!-- @import "[TOC]" {cmd="toc" depthFrom=1 depthTo=6 orderedList=false} -->
+
+<!-- code_chunk_output -->
+
+- [Getting Started with Create React App](#getting-started-with-create-react-app)
+  - [Available Scripts](#available-scripts)
+    - [`yarn start`](#yarn-start)
+    - [`yarn test`](#yarn-test)
+    - [`yarn build`](#yarn-build)
+    - [`yarn eject`](#yarn-eject)
+  - [Learn More](#learn-more)
+    - [Code Splitting](#code-splitting)
+    - [Analyzing the Bundle Size](#analyzing-the-bundle-size)
+    - [Making a Progressive Web App](#making-a-progressive-web-app)
+    - [Advanced Configuration](#advanced-configuration)
+    - [Deployment](#deployment)
+
+<!-- /code_chunk_output -->
+
+<!-- @import "[TOC]" {cmd="toc" depthFrom=1 depthTo=6 orderedList=false} -->
+
+<!-- code_chunk_output -->
+
+- [Getting Started with Create React App](#getting-started-with-create-react-app)
+  - [Available Scripts](#available-scripts)
+    - [`yarn start`](#yarn-start)
+    - [`yarn test`](#yarn-test)
+    - [`yarn build`](#yarn-build)
+    - [`yarn eject`](#yarn-eject)
+  - [Learn More](#learn-more)
+    - [Code Splitting](#code-splitting)
+    - [Analyzing the Bundle Size](#analyzing-the-bundle-size)
+    - [Making a Progressive Web App](#making-a-progressive-web-app)
+    - [Advanced Configuration](#advanced-configuration)
+    - [Deployment](#deployment)
+
+<!-- /code_chunk_output -->
+
 In the project directory, you can run:
 
 ### `yarn start`
@@ -64,7 +102,3 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/a
 ### Deployment
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `yarn build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { movies } from "./getMovies";
+import axios from "axios";
 
 export default class Movies extends Component {
     constructor() {
@@ -7,42 +7,137 @@ export default class Movies extends Component {
         this.state = {
             hover: "",
             parr: [1],
+            currPage: 1,
+            movies: [], // contains all movies
+            favourites: [],
         };
     }
 
-    render() {
-        let movie = movies.results;
+    // loadMoreMovies = async () => {
+    //     let newPage = this.state.currPage + 1;
+    //     const res = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=5540e483a20e0b20354dabc2d66a31c9&language=en-US&page=${newPage}`);
+    //     let data = res.data
+    //     // console.log(data);
+    //     this.setState({
+    //         movies: [...this.state.movies, ...data.results],
+    //         currPage: newPage
+    //     })
+    // }
 
+    // getting the movies for the very first time
+    async componentDidMount() {
+        //Side effects
+        const res = await axios.get(
+            `https://api.themoviedb.org/3/movie/popular?api_key=5540e483a20e0b20354dabc2d66a31c9&language=en-US&page=${this.state.currPage}`
+        );
+        let data = res.data;
+        // console.log(data);
+        this.setState({
+            movies: [...data.results],
+        });
+        // let callbackfn = (entries) => {
+        //     if (entries[0].isIntersecting) {
+        //         this.loadMoreMovies();
+        //     }
+        // }
+        // let loader = document.querySelector(".infinite-loader");
+        // let observer = new IntersectionObserver(callbackfn, { threshold: 1.0 });
+        // observer.observe(loader);
+    }
+
+    // calling this function when pagination happens.
+    // calling it with different valur of currPage(current page number).
+    changeMovies = async () => {
+        const res = await axios.get(
+            `https://api.themoviedb.org/3/movie/popular?api_key=5540e483a20e0b20354dabc2d66a31c9&language=en-US&page=${this.state.currPage}`
+        );
+
+        let data = res.data;
+        this.setState({
+            movies: [...data.results],
+        });
+    };
+
+    handleRight = () => {
+        let temparr = [];
+        for (let i = 1; i <= this.state.parr.length + 1; i++) {
+            temparr.push(i);
+        }
+        this.setState({
+            parr: [...temparr],
+            currPage: this.state.currPage + 1,
+        }, this.changeMovies);
+    };
+
+    handleLeft = () => {
+        if (this.state.currPage != 1) {
+            this.setState({
+                currPage: this.state.currPage - 1,
+            }, this.changeMovies);
+        }
+    };
+
+    handleClick = (value) => {
+        if (value != this.state.currPage) {
+            console.log("i am called");
+            this.setState({
+                currPage: value,
+            }, this.changeMovies);
+        }
+    };
+
+    handleFavourites = (movie) => {
+        let oldData = JSON.parse(localStorage.getItem("movies-app") || "[]");
+
+        if (this.state.favourites.includes(movie.id)) {
+            oldData = oldData.filter((m) => m.id != movie.id);
+        }
+        else {
+            oldData.push(movie);
+        }
+        localStorage.setItem("movies-app", JSON.stringify(oldData));
+
+        this.handleFavouritesState();
+    };
+
+    handleFavouritesState = () => {
+        let oldData = JSON.parse(localStorage.getItem("movies-app") || "[]");
+        let temp = oldData.map((movie) => movie.id);
+
+        this.setState({
+            favourites: [...temp],
+        });
+    };
+
+    render() {
         return (
             <>
-                {movie.length === 0 ? (
-                    <div className="spinner-border text-primary" role="status">
-                        <span className="visually-hidden">Loading...</span>
+                {this.state.movies.length == 0 ? (
+                    <div class="spinner-border text-primary" role="status">
+                        <span class="visually-hidden">Loading...</span>
                     </div>
                 ) : (
                     <div>
                         <h3 className="text-center">
                             <strong>Trending</strong>
                         </h3>
-
                         <div className="movies-list">
-                            {movie.map((movieObj, key) => (
+                            {this.state.movies.map((movieObj) => (
                                 <div
                                     className="card movies-card"
                                     onMouseEnter={() => this.setState({ hover: movieObj.id })}
                                     onMouseLeave={() => this.setState({ hover: "" })}
-                                    key={key}
                                 >
                                     <img
                                         src={`https://image.tmdb.org/t/p/original${movieObj.backdrop_path}`}
                                         alt={movieObj.title}
                                         className="card-img-top movies-img"
                                     />
-
+                                    {/* <div className="card-body"> */}
                                     <h5 className="card-title movies-title">
                                         {movieObj.original_title}
                                     </h5>
-
+                                    {/* <p class="card-text movies-text">{movieObj.overview}</p> */}
                                     <div
                                         className="button-wrapper"
                                         style={{
@@ -51,37 +146,46 @@ export default class Movies extends Component {
                                             justifyContent: "center",
                                         }}
                                     >
-                                        {this.state.hover === movieObj.id && (
-                                            <a className="btn btn-primary movies-button">
-                                                Go Somewhere
+                                        {this.state.hover == movieObj.id && (
+                                            <a
+                                                className="btn btn-primary movies-button"
+                                                onClick={() => this.handleFavourites(movieObj)}
+                                            >
+                                                {this.state.favourites.includes(movieObj.id)
+                                                    ? "Remove from favourites"
+                                                    : "Add to favourites"}
                                             </a>
                                         )}
                                     </div>
+                                    {/* </div> */}
                                 </div>
                             ))}
                         </div>
-
-                        <div
-                            className="infinite-loader"
-                            style={{ display: "flex", justifyContent: "center" }}
-                        >
-                            <h2>Load More Movies .........................</h2>
-                        </div>
-
+                        {/* <div className="infinite-loader" style={{ display: 'flex', justifyContent: 'center' }}>
+                                <h2>Load More Movies .........................</h2>
+                            </div> */}
                         <div style={{ display: "flex", justifyContent: "center" }}>
-                            {" "}
                             <nav aria-label="Page navigation example">
-                                <ul className="pagination">
-                                    <li className="page-item">
-                                        <a className="page-link">Previous</a>
+                                <ul class="pagination">
+                                    <li class="page-item">
+                                        <a class="page-link" onClick={this.handleLeft}>
+                                            Previous
+                                        </a>
                                     </li>
-                                    {this.state.parr.map((value, key) => (
-                                        <li className="page-item" key={key}>
-                                            <a className="page-link">{value}</a>
+                                    {this.state.parr.map((value) => (
+                                        <li class="page-item">
+                                            <a
+                                                class="page-link"
+                                                onClick={() => this.handleClick(value)}
+                                            >
+                                                {value}
+                                            </a>
                                         </li>
                                     ))}
-                                    <li className="page-item">
-                                        <a className="page-link">Next</a>
+                                    <li class="page-item">
+                                        <a class="page-link" onClick={this.handleRight}>
+                                            Next
+                                        </a>
                                     </li>
                                 </ul>
                             </nav>
